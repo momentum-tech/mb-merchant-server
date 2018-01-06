e8bf289d369cf6107ff0b7c992b7f490ddc63ffc
Ghost4Wandering <yyunjie9@163.com>
2018-01-06 17:25:47 +0800

    基础代码入库
    基础代码入库

---
+++ src/main/java/com/mmnttech/mb/merchant/server/controller/MerchantController.java
@@ -0,0 +1,18 @@
+package com.mmnttech.mb.merchant.server.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @类名 MerchantController
+ * @描述:
+ *   TODO
+ * @版权: Copyright (c) 2017 云南动量科技有限公司
+ * @创建人 James
+ * @创建时间 2018年1月6日 下午5:24:02
+ * @版本 v1.0
+ * 
+ */
+@Controller
+public class MerchantController {
+
+}
